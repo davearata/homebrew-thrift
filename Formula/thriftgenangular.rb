683a3cb55d59008e44f23decffdabb22e6e5f463
dave <dave.arata@dacatech.com>
2013-11-26 16:29:52 -0500

    upgrade thrifGenAngular 0.1.3

--- Formula/thriftgenangular.rb
+++ Formula/thriftgenangular.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Thriftgenangular < Formula
   homepage ''
-  url 'https://github.com/davearata/twitter-commons/releases/download/v0.1.2/thriftGenAngularService.pex-0.1.2.zip'
-  sha1 'f12c0b1cb73d6c5d0bcd2bef61572e5a0d7e18f5'
+  url 'https://github.com/davearata/twitter-commons/releases/download/v0.1.3/thriftGenAngularService.pex-0.1.3.zip'
+  sha1 '9bb3ee91b6db0052415fc18796f9d820a8e22353'
 
   # depends_on 'cmake' => :build
   depends_on :python
